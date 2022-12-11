cbe37e418b01df5b1a359c1d2f01cf76ed045f19
dianorlova <dianaorlova0101@gmail.com>
2022-12-11 16:09:22 +0300

    task-react-render-and-effects-commit

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,25 @@
+import { useState, useEffect } from 'react';
 import { subscribe, unsubscribe } from './resources/API';
 
 export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+    const [message, setMessage] = useState(-1);
+
+    const callback = (sourceMessage: number) => {
+        setMessage(sourceMessage);
+    };
+
+    useEffect(() => {
+        setMessage(-1);
+        subscribe(props.sourceId, callback);
+
+        return () => {
+            unsubscribe(props.sourceId, callback);
+        };
+    }, [props.sourceId]);
+
+    return (
+        <div>
+            {props.sourceId}: {message}
+        </div>
+    );
 }
